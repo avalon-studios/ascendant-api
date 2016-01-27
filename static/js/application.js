a0ba936b27938085e1fe0dbe3877f53106caabf8
Elliot Greenlee <egreenle@vols.utk.edu>
2016-01-27 11:38:47 -0500

    Change append to prepend

--- static/js/application.js
+++ static/js/application.js
@@ -3,7 +3,7 @@ var outbox = new ReconnectingWebSocket("wss://"+ location.host + "/submit");
 
 inbox.onmessage = function(message) {
   var data = JSON.parse(message.data);
-  $("#chat-text").append("<div class='panel panel-default'><div class='panel-heading'>" + $('<span/>').text(data.handle).html() + "</div><div class='panel-body'>" + $('<span/>').text(data.text).html() + "</div></div>");
+  $("#chat-text").prepend("<div class='panel panel-default'><div class='panel-heading'>" + $('<span/>').text(data.handle).html() + "</div><div class='panel-body'>" + $('<span/>').text(data.text).html() + "</div></div>");
   $("#chat-text").stop().animate({
     scrollTop: $('#chat-text')[0].scrollHeight
   }, 800);
