102b7946519011a1f9f6c693c25ebc454bd1537d
Kyle Bashour <kylebshr@me.com>
2016-02-25 23:41:10 -0500

    Add missing self

--- server.py
+++ server.py
@@ -101,7 +101,7 @@ class Game(object):
         for player in self.players:
             json_players.append({'id': player.id, 'name': player.name, 'team': player.team})
 
-        socketio.emit('update_players', json_players, json=True, room=game_id)
+        socketio.emit('update_players', json_players, json=True, room=self.game_id)
 
 class Player(object):
     """Game player class"""
